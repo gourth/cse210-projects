1292819b68949007872bdc8c61824b51c157f596
Garrett Ourth <garrettourth@gmail.com>
2023-01-13 18:03:56 -0600

    add CSV help code

--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -1,9 +1,42 @@
 using System;
+using System.IO;
+using System.Text;
 
 class Program
 {
+
+    string filePath = "C:\\temp\\CSVTest.csv";
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop02 World!");
+        Program program = new Program();
+
+        program.writeToCSV();
+        program.readFromCSV();
     }
+
+    public void writeToCSV()
+
+    // Write CSV file
+    StringBuilder builder = new StringBuilder();
+
+    string text1 = "Text1";
+    string text2 = "Text2";
+    Builder.AppendLine(string.Format("{0}|{1}", text1, text2));
+
+    File.WrtieAllText(filePath, UriBuilder.ToString());
+
+    public void readFromCSV()
+    {
+        StreamReader streamReader = new StreamReader(filePath);
+
+        while (!streamReader.EndOfStream)
+        {
+            var line = streamReader.ReadLine();
+            var values = line.Split();
+
+            Console.WriteLine("{0}|{1}", values[0], values[1]);
+        }
+    }
+
+
 }
